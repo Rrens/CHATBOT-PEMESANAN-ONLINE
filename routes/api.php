a5d0c7630d4fdbcc3895a428bdaf605ad1376347
Rrens <yusufrendi313@gmail.com>
2024-01-21 05:01:45 +0700

    asd

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/saving-recomendation-data', [RecomendationController::class, 'index']);
+Route::get('/cek', function () {
+    return response()->json('cek');
+});
